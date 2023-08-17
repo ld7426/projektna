1363be6a9649dba8c5e9c4bc1f496f2c36bd2cae
Lovro Drofenik <ld7426@student.uni-lj.si>
2023-08-17 14:20:47 +0200

    Urejanje slovarja, csv, šumniki

--- prenos_podatkov.py
+++ prenos_podatkov.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
+import csv
 
 website = "https://www.arso.gov.si/vode/podatki/amp/Ht_30.html" #spletna stran iz katere potegnemo prve podatke
 result = requests.get(website)
@@ -11,7 +12,7 @@ soup = bs(content, "lxml") #rad imam juho al nekaj
 
 #print(content)
 
-slovar = dict({})
+slovar = []
 
 deljensoup = content.split(">") #razdelim na vrstice
 uporabnevrstice = []
@@ -28,9 +29,21 @@ for indeks, vrstica in enumerate(uporabnevrstice):
 for vrstica in uporabnevrstice:
     zacasna=vrstica.split("&#10")[0][1:]
     id = vrstica[-18:-3]
-    slovar[id] = zacasna.split(" - ")
+    #slovar[id] = zacasna.split(" - ")
+
+    slovar.append({"Ime": zacasna.split(" - ")[0].replace("&#x010D;", "č").replace("&#x010C;", "Č").replace("&#x0160;", "Š").replace("&#x0161;", "š").replace("&#x017E;", "ž").replace("&#x017D;", "Ž"), "Reka":zacasna.split(" - ")[1].replace("&#x010D;", "č").replace("&#x010C;", "Č").replace("&#x0160;", "Š").replace("&#x0161;", "š").replace("&#x017E;", "ž").replace("&#x017D;", "Ž"), "ID":id})
 
 print(slovar) #dobljen slovar povezuje imena postaj in identifikatorje
 
 #zdaj lahko začnemo z downloadanjem podatkov posameznih postaj
-
+polja = ["Ime", "Reka", "ID"]
+with open("slovar.csv", "w", newline="") as csvfile:
+    # Create a CSV writer using the field/column names
+    writer = csv.DictWriter(csvfile, fieldnames=polja)
+    
+    # Write the header row (column names)
+    writer.writeheader()
+    
+    # Write the data
+    for row in slovar:
+        writer.writerow(row)
