73fcc790babe0f08b60a1a968acd65287db6ed17
Lovro Drofenik <ld7426@student.uni-lj.si>
2023-08-28 11:40:23 +0200

    Popravljena napaka v pren-identf, če ima ime 2x -

--- prenos-identifikatorjev.py
+++ prenos-identifikatorjev.py
@@ -27,12 +27,15 @@ for indeks, vrstica in enumerate(uporabnevrstice):
     uporabnevrstice[indeks] = str(vrstica).split('alt=')[1] #žeim extractat ime, vrstica s torej začne z imenom, ki pa ima še " spredaj, skoraj ziher je to daleč od optimalnega :D
 
 for vrstica in uporabnevrstice:
-    zacasna=vrstica.split("&#10")[0][1:]
-    id = vrstica[-18:-3]
-    #slovar[id] = zacasna.split(" - ")
-
-    slovar.append({"Ime": zacasna.split(" - ")[0].replace("&#x010D;", "č").replace("&#x010C;", "Č").replace("&#x0160;", "Š").replace("&#x0161;", "š").replace("&#x017E;", "ž").replace("&#x017D;", "Ž"), "Reka":zacasna.split(" - ")[1].replace("&#x010D;", "č").replace("&#x010C;", "Č").replace("&#x0160;", "Š").replace("&#x0161;", "š").replace("&#x017E;", "ž").replace("&#x017D;", "Ž"), "ID":id})
+    zacasna=vrstica.split("&#10")[0][1:] #ta znak je bil na koncu vsakega podatka zato splittam po njem
+    zacasna = zacasna.replace("&#x010D;", "č").replace("&#x010C;", "Č").replace("&#x0160;", "Š").replace("&#x0161;", "š").replace("&#x017E;", "ž").replace("&#x017D;", "Ž") 
     #tale koda je ugly af, ampak deluje, tak da se ne bom ukvarjal z drugim, zamenja pa čudne characterje s šumniki, ker so čudni characterji motili, na primer imeli so ; notr
+    zacasna = zacasna.split(" - ")
+    id = vrstica[-18:-3]
+    
+    slovar.append({"Ime": zacasna[0], "Reka":zacasna[-1], "ID":id})
+    
+
 #print(slovar) #dobljen slovar povezuje imena postaj, reko in identifikatorje
 
 #zdaj lahko začnemo z downloadanjem podatkov posameznih postaj
