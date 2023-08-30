da1ae82e3b3704322eac66874d16e8e0d32f8bac
Lovro Drofenik <ld7426@student.uni-lj.si>
2023-08-31 01:16:53 +0200

    Izbrisane nepotrebne datoteke

--- urlji.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-reqs = requests.get("https://www.arso.gov.si/vode/podatki/amp/Ht_30.html")
-soup = BeautifulSoup(reqs.text, 'html.parser')
- 
-urls = []
-for link in soup.find_all('a'):
-    print(link.get('href'))
